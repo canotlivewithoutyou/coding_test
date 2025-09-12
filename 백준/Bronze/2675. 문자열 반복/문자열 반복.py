23fe33337084ecf1a6f80bd463793f5ea229abbb
canotlivewithoutyou <164298145+canotlivewithoutyou@users.noreply.github.com>
2025-09-12 18:55:44 +0900

    [Bronze II] Title: 문자열 반복, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.py
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -1,12 +1,8 @@
 import sys
-k=int(input())
-i=0
-result=''
-while i<k:
-    a,b=sys.stdin.readline().split()
-    b_line=list(b.rstrip('\n'))
-    for j in range(0,len(b_line)):
-        result+=b_line[j]*int(a)
-    print(result)
-    result=''
-    i+=1
+k=int(sys.stdin.readline())
+for _ in range(k):
+    a, b = sys.stdin.readline().split()
+    result=[]
+    for ch in b:
+        result.append(ch*int(a))
+    print(''.join(result))
