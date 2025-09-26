0122cf72af7820b3fbaea841c73784cb054cf437
canotlivewithoutyou <164298145+canotlivewithoutyou@users.noreply.github.com>
2025-09-26 15:50:09 +0900

    [Bronze II] Title: 블랙잭, Time: 76 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -0,0 +1,12 @@
+import sys
+
+n,m=map(int, sys.stdin.readline().split())
+nums=list(map(int, sys.stdin.readline().split()))
+max=0
+for i in range(n-2):
+    for j in range(i+1, n-1):
+        for k in range(j+1, n):
+            sum=nums[i]+nums[j]+nums[k]
+            if sum<=m and sum>max:
+                max=sum
+print(max)
