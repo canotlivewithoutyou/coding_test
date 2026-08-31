a04c0fa04a02c21fef9bcd3ef852e47d45f252da
Suhyun <164298145+canotlivewithoutyou@users.noreply.github.com>
2026-08-31 16:00:10 +0900

    [D1] Title: 은행 번호표 발급기, Time: 88 ms, Memory: 60,800 KB -BaekjoonHub

---
+++ SWEA/D1/27023. 은행 번호표 발급기/README.md
@@ -0,0 +1,15 @@
+# [D1] 은행 번호표 발급기 - 27023 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AZ87c2tayF3HBITH) 
+
+### 성능 요약
+
+메모리: 60,800 KB, 시간: 88 ms, 코드길이: 390 Bytes
+
+### 제출 일자
+
+2026-08-31 16:00
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D1/27023. 은행 번호표 발급기/은행 번호표 발급기.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+T= int(input())
+
+for tc in range(1, T+1):
+    M = int(input())
+    inpt = list(map(int, input().split()))
+    
+    queue = deque()
+    number = 1
+    result = []
+    
+    for k in inpt:
+        if k == 1:
+            queue.append(number)
+            number+=1
+        
+        else:
+            result.append(queue.popleft())
+    
+    print(f'#{tc}', *result)
