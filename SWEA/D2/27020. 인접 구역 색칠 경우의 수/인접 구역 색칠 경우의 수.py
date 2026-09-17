942aa4450a05c32035203a6972e2e5100a3db7d6
Suhyun <164298145+canotlivewithoutyou@users.noreply.github.com>
2026-09-17 09:43:24 +0900

    [D2] Title: 인접 구역 색칠 경우의 수, Time: 128 ms, Memory: 61,568 KB -BaekjoonHub

--- SWEA/D2/27020. 인접 구역 색칠 경우의 수/인접 구역 색칠 경우의 수.py
+++ SWEA/D2/27020. 인접 구역 색칠 경우의 수/인접 구역 색칠 경우의 수.py
@@ -4,24 +4,24 @@ def dfs(idx):
     if idx == (N+1):
         answer+=1
         return 
-    
-    for c in range(1, K+1): # c: color
+
+    for current_color in range(1, K+1):
         possible = True
 
         for next_node in graph[idx]:
-
-            if color[next_node]==c:
-                possible=False
+            if color[next_node] == current_color:
+                possible = False
                 break
 
         if possible:
-            color[idx]=c
+            color[idx] = current_color
             dfs(idx+1)
             color[idx]=0
 
-T= int(input())
+T = int(input())
 
 for tc in range(1, T+1):
+
     N, M, K = map(int, input().split())
 
     graph = [[] for _ in range(N+1)]
@@ -33,7 +33,7 @@ for tc in range(1, T+1):
         graph[b].append(a)
 
     color = [0]*(N+1)
-    answer=0
+    answer = 0
 
     dfs(1)
 
